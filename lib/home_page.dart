8066dd52e045c992e4646f81d67bd21e257531f8
Shashank Tripathi <shashank.tripathi.da7844@gmail.com>
2021-11-15 10:44:19 +0530

    First Commit

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome all in $days days of flutter"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
